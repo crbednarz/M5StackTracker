f686a78cb492f71c9bde615ec65bb8126f1e50f7
Skiphs <cr.bednarz@gmail.com>
2018-04-12 19:33:48 -0400

    Program now report framerate via printf

--- main/Program.c
+++ main/Program.c
@@ -8,8 +8,10 @@
 #include "Motion.h"
 #include "Input.h"
 #include "I2C.h"
+#include "Performance.h"
 
-Game ActiveGame;
+static Game ActiveGame;
+static Stopwatch Timer;
 
 static void DisableAudio()
 {
@@ -37,9 +39,19 @@ void app_main()
 	InitializeSystems();
 
 	Game_Initialize(&ActiveGame);
+	Stopwatch_Reset(&Timer);
 	while (1)
 	{
+		Stopwatch_Start(&Timer);
 		Game_Update(&ActiveGame);
 		Game_Render(&ActiveGame);
+		Stopwatch_End(&Timer);
+
+		if (Timer.RecordCount == 100)
+		{
+			float averageTimeElapsed = Stopwatch_AverageTime(&Timer);
+			printf("Average Frame Time: %ffps\n", 1.0 / averageTimeElapsed);
+			Stopwatch_Reset(&Timer);
+		}
 	}
 }
